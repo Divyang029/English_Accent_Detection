bc1992eef819a45c72f7bd0d2d3c6786e2cf8732
Divyang Dhameliya <dhameliyadivyang@gmail.com>
2025-01-05 21:27:44 +0530

    initial commit

---
+++ backend/SpringBoot_backend/AccentDetection/src/main/java/com/example/AccentDetection/AccentDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.AccentDetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccentDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccentDetectionApplication.class, args);
+	}
+
+}
---
+++ backend/SpringBoot_backend/AccentDetection/src/test/java/com/example/AccentDetection/AccentDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AccentDetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccentDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
