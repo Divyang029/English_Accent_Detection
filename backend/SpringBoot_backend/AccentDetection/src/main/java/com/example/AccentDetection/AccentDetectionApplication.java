e8e9ab1feec77a8e1b9a99db192977617a1a0d1a
Divyang Dhameliya <dhameliyadivyang@gmail.com>
2025-05-18 19:26:24 +0530

    Fix for env variables

--- backend/SpringBoot_backend/AccentDetection/src/main/java/com/example/AccentDetection/AccentDetectionApplication.java
+++ backend/SpringBoot_backend/AccentDetection/src/main/java/com/example/AccentDetection/AccentDetectionApplication.java
@@ -2,13 +2,13 @@ package com.example.AccentDetection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class AccentDetectionApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+//		Dotenv dotenv = Dotenv.load();
+//		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(AccentDetectionApplication.class, args);
 	}
 }
